d0e7b137f9a6d204a052c01b98924c1054f21b56
Seiji Sonoda <seijisonoda01180627@gmail.com>
2021-06-13 22:25:55 +0900

    Initial Commit

---
+++ MentorBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentorBook
+//
+//  Created by 園田成二 on 2021/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
